9517fccbea40b5a334b16efe6d2ae190f35eb500
Brandon <bcordell@raysbaseball.com>
2023-06-12 17:45:40 -0400

    Add link

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
 <template>
   <router-view/>
+  <footer class="absolute bottom-0 left-0">
+    <router-link to="/time_entries/new" class="text-white" target="_blank">New Time Entries</router-link>
+  </footer>
 </template>
